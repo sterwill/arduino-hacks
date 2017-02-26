2c05bc4fa5aabfb9c708b352794f737f64572421
Shaw Terwilliger <sterwill@tinfig.com>
2017-02-26 17:44:04 -0500

    Use PWM

--- powerbox/powerbox.ino
+++ powerbox/powerbox.ino
@@ -1,109 +1,79 @@
-#include <math.h>
+#include <Arduino.h>
 
 /* HARDWARE */
 
-// Normally CLOSED relay
+// Pin must support PWM.
 #define POWER_RELAY_PIN        9
+
+// Define this value for normally closed relays; #undef if normally open.
+#define POWER_RELAY_NORMALLY_CLOSED
+
+// Pin must support PWM.
 #define POWER_LED_PIN          11
 
-#define THERMAL_RESISTOR_PIN   A0
+// Linear taper potentiometer / variable resistor.
 #define POT_PIN                A1
 
 // Measured min and max readings
-#define POT_CALIB_MIN          105
+#define POT_CALIB_MIN          100
 #define POT_CALIB_MAX          1023
+#define POT_CALIB_RANGE        (POT_CALIB_MAX - POT_CALIB_MIN)
+
+void print(const char * format, ...) {
+  char buf[1024];
+  va_list argptr;
+  
+  va_start(argptr, format);
+  int len = vsnprintf(buf, sizeof(buf), format, argptr);
+  va_end(argptr);
+  Serial.write(buf, len);
+}
 
 /* CONTROL PARAMETERS */
+void set_led(uint8_t value) {
+  analogWrite(POWER_LED_PIN, value);
+}
 
-// The power cycle length is the on plus off parts
-#define POWER_CYCLE_MILLIS     10000
+void set_power(uint8_t value) {
+#ifdef POWER_RELAY_NORMALLY_CLOSED
+  value = 255 - value;
+#endif
+  analogWrite(POWER_RELAY_PIN, value);
+}
 
-static unsigned long prev_loop_time = 0;
+/* Returns a normalized value (in the range 0-255) from POT_PIN. */
+uint8_t read_pot() {
+  uint16_t raw = (uint16_t) analogRead(POT_PIN);
+  print("raw = %04d [%d-%d], ", raw, POT_CALIB_MIN, POT_CALIB_MAX);
 
-// Counts from 0 to POWER_CYCLE_MILLIS and resets
-static unsigned long power_cycle_time = 0;
+  // Scale from calibration min and max.
+  if (raw < POT_CALIB_MIN) {
+    raw = POT_CALIB_MIN;
+  } else if (raw > POT_CALIB_MAX) {
+    raw = POT_CALIB_MAX;
+  } else {
+    raw = raw - POT_CALIB_MIN;
+  }
+  uint8_t scaled = 255 * ((float) raw / (float) POT_CALIB_RANGE);
 
-// True when the switched outlets are on, false when they are off 
-// (relay is normally CLOSED so default is true)
-static boolean power_enabled = true;
+  print("scaled = %03d\n", scaled);
+  return scaled;
+}
 
 void setup() {
   Serial.begin(115200);
 
+  pinMode(POT_PIN, INPUT);
   pinMode(POWER_RELAY_PIN, OUTPUT);
   pinMode(POWER_LED_PIN, OUTPUT);
 
-  pinMode(THERMAL_RESISTOR_PIN, INPUT);
-  pinMode(POT_PIN, INPUT);
-  
-  power(false);
+  set_power(0);
+  set_led(0);
 }
 
 void loop() {
-  // Reads [0.0,1.0], higher values mean longer duty cycles
-  float pot_percent = read_pot();
-  
-  // Calculate how much of the total cycle is "on"
-  float power_cycle_on_millis = pot_percent * POWER_CYCLE_MILLIS;
-
-  unsigned long time = millis();
-  unsigned long delta = time - prev_loop_time;
-  
-  // Account for elapsed time this loop
-  power_cycle_time += delta;
-  if (power_cycle_time > POWER_CYCLE_MILLIS) {
-    power_cycle_time = 0;
-  }
-  
-  // Turn on power if in the "on" part
-  power(power_cycle_time <= power_cycle_on_millis);
-  
-  prev_loop_time = time;
-  
-  delay(100);
+  uint8_t value = read_pot();
+  set_power(value);
+  set_led(value);
 }
 
-/* Turns the switched power outlets and indicator LED on or off. */
-void power(boolean on) {
-  if (on == power_enabled) {
-    return;
-  }
-  
-  if (on) {
-    // Relay is normally closed, so go LOW to let the juice flow
-    digitalWrite(POWER_RELAY_PIN, LOW);
-    digitalWrite(POWER_LED_PIN, HIGH);
-  } else {
-    // Relay is normally closed, so go HIGH to cut the juice
-    digitalWrite(POWER_RELAY_PIN, HIGH);
-    digitalWrite(POWER_LED_PIN, LOW);
-  }
-  power_enabled = on;
-}
-
-float cbrt(float f) {
-  return pow(f, 1.0/3.0);
-}
-
-/* Returns a value in the range [0.0,1.0] from POT_PIN, normalized to linear response. */
-float read_pot() {
-  int raw = analogRead(POT_PIN);
-  Serial.print("Pot raw: ");
-  Serial.println(raw);
-
-  const float scale = (1023.0 / (float) (POT_CALIB_MAX - POT_CALIB_MIN));
-  float scaled = scale * (raw - POT_CALIB_MIN);
-  if (scaled < 0) {
-    scaled = 0;
-  } else if (scaled > 1023) {
-    scaled = 1023;
-  }
-  Serial.print("Pot scaled: ");
-  Serial.println(scaled);
-
-  float percent = scaled / 1024.0;
-  Serial.print("Pot percent: ");
-  Serial.println(percent);
-  
-  return percent;
-}
